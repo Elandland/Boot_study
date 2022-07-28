ccc863cbef0bb542162c96c635631242dada9d09
elandland <bodan99@gmail.com>
2022-07-28 15:09:38 +0900

    DB 연동하기

---
+++ src/main/java/boot/camp/boot_study/service/UserService.java
@@ -0,0 +1,19 @@
+package boot.camp.boot_study.service;
+
+import boot.camp.boot_study.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserMapper userMapper;
+
+    public ArrayList<HashMap<String, Object>> findAll() {
+        return userMapper.findAll();
+    }
+}
